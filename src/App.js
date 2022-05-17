fb3ca40b094f10ba19f697054b939b1991f2bf7f
jgranados4 <xavier10001985@gmail.com>
2022-05-16 19:48:30 -0500

    mi primer proyecto finalizado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
 import Testimonio from './componentes/Testimonio';
 
-function App() {
-  return (
-    <div className="App">
+// function App() {
+//   return (
+//   );
+// }
+const App= ()=>{
+  return(
+     <div className="App">
       <div className='contenido-principal'>
-        <h1>Esto dice nuestro alumnos</h1>
+        <h1>Esto dice nuestro alumnos:</h1>
       </div>
-      <Testimonio />
+      <Testimonio
+        nombre='Emma'
+        pais='Ecuador'
+        cargo='Ingeniera de sistemas'
+        imagen='ima'
+        empresa='Facebook'
+        Testimonio='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'/>
+       < Testimonio
+        nombre='Juan'
+        pais='Peru'
+        cargo='Ingeniero de sistemas'
+        imagen='ima'
+        empresa='Facebook'
+        Testimonio='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'/>
     </div>
   );
 }
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,36 +1,25 @@
-import React from 'react';
-import '../style/Testimonio.css';
+import "../style/Testimonio.css";
 
- function Testimonio() {
-    return (
-      <div className="contenedor-testimonio">
-        <img
-          className="imagenes-testimonio"
-          src={require("../imagenes/ima.jpg")}
-          alt="foto de emma"
-        />
-        <div className="contenedor-textotestimonio">
-          <p className="nombretestimonio">Emma bostion en sucia</p>
-          <p className="cargotestimonio">Ingeniero de software</p>
-          <p className="texto-testimonio">
-            Lorem Ipsum es simplemente el texto de relleno de las imprentas y
-            archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar
-            de las industrias desde el año 1500, cuando un impresor (N. del T.
-            persona que se dedica a la imprenta) desconocido usó una galería de
-            textos y los mezcló de tal manera que logró hacer un libro de textos
-            especimen. No sólo sobrevivió 500 años, sino que tambien ingresó
-            como texto de relleno en documentos electrónicos, quedando
-            esencialmente igual al original. Fue popularizado en los 60s con la
-            creación de las hojas "Letraset", las cuales contenian pasajes de
-            Lorem Ipsum, y más recientemente con software de autoedición, como
-            por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem
-            Ipsum.
-          </p>
-        </div>
+function Testimonio(props) {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className="imagenes-testimonio"
+        src={require(`../imagenes/${props.imagen}.jpg`)}
+        alt="foto de emma"
+      />
+      <div className="contenedor-textotestimonio">
+        <p className="nombretestimonio">
+          {props.nombre} en {props.pais}
+        </p>
+        <p className="cargotestimonio">
+          {props.cargo}en{props.empresa}
+        </p>
+        <p className="texto-testimonio">"{props.Testimonio}"</p>
       </div>
-    );
+    </div>
+  );
 }
-
 
 // const testimonio = ()=>{
 // }
